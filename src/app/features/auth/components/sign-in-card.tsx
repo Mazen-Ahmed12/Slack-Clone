d7406c67a3f8484363d675bae325f4e7e2a74f53
MDmazen <mazena083@gmail.com>
2025-01-17 14:15:11 +0200

    feat(sign-up page)
    creating sign-up page with adding event listener to both sign-up and sign-in pages

--- src/app/features/auth/components/sign-in-card.tsx
+++ src/app/features/auth/components/sign-in-card.tsx
@@ -1,85 +1,95 @@
+import { useState } from "react";
 import {
-    Button,
-    Card,
-    CardContent,
-    CardHeader,
-    Divider,
-    OutlinedInput,
-    Typography,
-    CardActions,
-  } from "@mui/material";
-  import { Google, GitHub } from "@mui/icons-material";
-  import { SignInFlow } from "../types";
-  
-  interface SignInCardProps {
-    setState: (state: SignInFlow) => void;
-  }
-  
-  export const SignInCard = ({setState} :SignInCardProps) => {
-    return (
-      <Card className="w-full h-full p-8 text-xl font-bold">
-        <CardHeader title="Login to continue" className="px-0 pt-0" />
-        <CardContent className="space-y-5">
-          <Typography>Use your email or another service to continue</Typography>
-          <form className="space-y-2.5">
-            <OutlinedInput
-              disabled={false}
-              value=""
-              onChange={() => {}}
-              placeholder="Email"
-              type="email"
-              required
-              fullWidth
-            />
-            <OutlinedInput
-              disabled={false}
-              value=""
-              onChange={() => {}}
-              placeholder="password"
-              type="password"
-              required
-              fullWidth
-            />
-            <Button
-              type="submit"
-              className="w-full bg-slate-900 text-white hover:bg-gray-800 normal-case"
-              size="large"
-              disabled={false}
-            >
-              continue
-            </Button>
-          </form>
-          <Divider variant="middle" />
-          <CardActions className="flex flex-col gap-y-2.5">
-            <Button
-              disabled={false}
-              onClick={() => {}}
-              variant="outlined"
-              size="large"
-              className="w-full relative text-black border-white hover:bg-neutral-300"
-            >
-              <Google className="size-5 absolute top-2.1 left-2.5 " />
-              Continue with Google
-            </Button>
-            <Button
-              disabled={false}
-              onClick={() => {}}
-              variant="outlined"
-              size="large"
-              className="w-full relative !ml-0 text-black border-white hover:bg-neutral-300"
-            >
-              <GitHub className="size-5 absolute top-2 left-2.5" />
-              Continue with githup
-            </Button>
-          </CardActions>
-          <Typography className="text-s text-muted-foreground inline-block" component="span">
-            Don&apos;t have an account? {/* we used (&apos;) instead of (')*/}{" "}
-            <Typography onClick={()=>setState("signUp")} className="text-sky-700 hover:underline cursor-pointer inline-block" component="span">
-              Sign up
-            </Typography>
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Divider,
+  OutlinedInput,
+  Typography,
+  CardActions,
+} from "@mui/material";
+import { Google, GitHub } from "@mui/icons-material";
+import { SignInFlow } from "../types";
+
+interface SignInCardProps {
+  setState: (state: SignInFlow) => void;
+}
+
+export const SignInCard = ({ setState }: SignInCardProps) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <Card className="w-full h-full p-6 text-xl font-bold">
+      <CardHeader title="Login to continue" className="px-0 pt-0" />
+      <CardContent className="space-y-5">
+        <Typography>Use your email or another service to continue</Typography>
+        <form className="space-y-2.5">
+          <OutlinedInput
+            disabled={false}
+            value={email}
+            onChange={(e) => {setEmail(e.target.value)}}
+            placeholder="Email"
+            type="email"
+            required
+            fullWidth
+          />
+          <OutlinedInput
+            disabled={false}
+            value={password}
+            onChange={(e) => {setPassword(e.target.value)}}
+            placeholder="password"
+            type="password"
+            required
+            fullWidth
+          />
+          <Button
+            type="submit"
+            className="w-full bg-slate-900 text-white hover:bg-gray-800 normal-case"
+            size="large"
+            disabled={false}
+          >
+            continue
+          </Button>
+        </form>
+        <Divider variant="middle" />
+        <CardActions className="flex flex-col gap-y-2.5">
+          <Button
+            disabled={false}
+            onClick={() => {}}
+            variant="outlined"
+            size="large"
+            className="w-full relative text-black border-white hover:bg-neutral-300"
+          >
+            <Google className="size-5 absolute top-2.1 left-2.5 " />
+            Continue with Google
+          </Button>
+          <Button
+            disabled={false}
+            onClick={() => {}}
+            variant="outlined"
+            size="large"
+            className="w-full relative !ml-0 text-black border-white hover:bg-neutral-300"
+          >
+            <GitHub className="size-5 absolute top-2 left-2.5" />
+            Continue with githup
+          </Button>
+        </CardActions>
+        <Typography
+          className="text-s text-muted-foreground inline-block"
+          component="span"
+        >
+          Don&apos;t have an account? {/* we used (&apos;) instead of (')*/}{" "}
+          <Typography
+            onClick={() => setState("signUp")}
+            className="text-sky-700 hover:underline cursor-pointer inline-block"
+            component="span"
+          >
+            Sign up
           </Typography>
-        </CardContent>
-      </Card>
-    );
-  };
-  
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
